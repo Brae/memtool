a9d0f9de991f90923377d584110cb02738a7d37f
GeneralBrae <pedrocowman@gmail.com>
2015-07-19 19:45:00 +0100

    Function Headers
    Changed function names

--- Memory Tool.cpp
+++ Memory Tool.cpp
@@ -8,14 +8,14 @@
 #include <time.h>
 using namespace std;
 
-DWORD WINAPI MyFunctionA(LPVOID lpParam) {
+DWORD WINAPI DumpHandler(LPVOID lpParam) {
 	time_t timer;
 	system("winpmem_2.0.1 -o output_test1.aff4");
 	//cout << "winpmem_2.0.1 -o output" << time(&timer) << ".aff4";
 	return 0;
 }
 
-DWORD WINAPI MyFunctionB(LPVOID lpParam) {
+DWORD WINAPI Analyser(LPVOID lpParam) {
 	cout << "Thread 2 reporting in!" << "\n";
 
 	return 0;
@@ -26,8 +26,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	DWORD dwThreads[2];
 	HANDLE hThreads[2];
 
-	hThreads[0] = CreateThread(NULL, 0, MyFunctionA, NULL, 0, &dwThreads[0]);
-	hThreads[1] = CreateThread(NULL, 0, MyFunctionB, NULL, 0, &dwThreads[1]);
+	hThreads[0] = CreateThread(NULL, 0, DumpHandler, NULL, 0, &dwThreads[0]);
+	hThreads[1] = CreateThread(NULL, 0, Analyser, NULL, 0, &dwThreads[1]);
 
 	for (int i = 0; i < 2; i++) {
 		if (hThreads[i] == NULL) {
