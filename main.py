99bf1bc68cc4130e1d4358b0993c4a0709befe35
Brae <pedrocowman@gmail.com>
2015-08-19 16:55:24 +0100

    Small updates

--- main.py
+++ main.py
@@ -7,18 +7,42 @@ import subprocess
 import sys
 import os
 
-def searchmem():
-    #TODO
-    
-def readmem():
-    #TODO
+def readmem(fd, start, size):
+    win32file.SetFilePointer(fd, start, 0 )
+    x,data = win32file.ReadFile(fd, size)
+    return data
 
-def menu():
+def searchmem(fd):
+    #WIP
+    print "\n=============================\n=     Search for string     =\n"
+    start = input("\tEnter start address for search: ")
+    end = input("\tEnter end address for search: ")
+    srch = raw_input("\tEnter the string to search for: ")
+    results = []
+    for pointer in range(start, end, 1024*1024): #step through memory 1MB at a time
+        seg = readmem(fd, pointer, 1024*1024)
+        if srch in seg and not "msrch(" in seg:
+            offset = seg.index(srch)
+            results.append(pointer+offset)
+        if srch.encode("utf-16le") in seg and not "msrch(" in seg:
+            offset = seg.index(srch.encode("utf-16le"))
+            srchres.append(pointer+seg.index(srch.encode("utf-16le")))
+        if srch.encode("utf-16be") in seg and not "msrch(" in seg:
+            offset = seg.index(srch.encode("utf-16be"))
+            srchres.append(pointer+x.index(srch.encode("utf-16be")))
+    for a in results:
+        print "\n-----------------------------\n", a, "\n"
+    return
+
+
+
+def menu(fd):
     print "\n=============================\n"
     print "Menu:\n\t1 - Search memory for string\n\t2 - Read section of memory"
     c = raw_input("\t> ")
     if c == '1':
         print "\n\t[i] Launching 'searchmem()'..."
+        searchmem(fd)
     elif c == '2':
         print "\n\t[i] Launching 'readmem()'..."
     else:
@@ -37,7 +61,7 @@ else:
 
 print "[i] Initialising file"
 fd = win32file.CreateFile(r"\\.\pmem",win32file.GENERIC_READ | win32file.GENERIC_WRITE,win32file.FILE_SHARE_READ | win32file.FILE_SHARE_WRITE,None,win32file.OPEN_EXISTING,win32file.FILE_ATTRIBUTE_NORMAL,None)
-menu()
+menu(fd)
 
 
 
